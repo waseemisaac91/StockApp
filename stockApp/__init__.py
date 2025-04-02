f901709524a7857a6072b823e4c35ca657c57f9c
waseem <waseem.isaac.2016@gmail.com>
2025-04-02 06:41:39 -0700

    Initial Django project commit

---
+++ stockApp/__init__.py
@@ -0,0 +1,5 @@
+git add .
+git commit -m "Initial website commit"
+git branch -M main
+git remote add origin https://github.com/waseemisaac91/StockApp.git
+git push -u origin main
---
+++ stockApp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import path,include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('stockPro.urls', namespace='stockPro')),
+] +static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
